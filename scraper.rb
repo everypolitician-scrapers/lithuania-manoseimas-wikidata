10e2770f56736056a06aa8f910fd5e719e8206f5
Tony Bowden <tony@tmtm.com>
2019-01-30 08:39:11 +0000

    Fix dewiki link
    The page has moved

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,7 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_Seimas_2012–2016',
+  url: 'https://de.wikipedia.org/wiki/11._Seimas',
   before: '//span[@id="Weblinks"]',
   xpath: '//li/a[not(@class="new")][1]/@title',
 ) 
